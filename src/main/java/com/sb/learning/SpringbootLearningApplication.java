f6093dc4f3f347e1159cf6d867f6d1cfd46a7ce9
someone <someone@someplace.com>
2021-09-02 12:01:20 +0530

    added files

---
+++ src/main/java/com/sb/learning/SpringbootLearningApplication.java
@@ -0,0 +1,16 @@
+package com.sb.learning;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLearningApplication.class, args);
+
+	}
+
+}
---
+++ src/main/java/com/sb/learning/model/Student.java
@@ -0,0 +1,34 @@
+package com.sb.learning.model;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Component
+@RestController
+@RequestMapping("/students")
+public class Student {
+	
+	int id;
+	String name;
+	String branch;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+
+}
